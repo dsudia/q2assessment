eb58edd067232b8e90aa728ea2379f1b6872c914
David Sudia <dsudia@gmail.com>
2016-03-18 09:46:00 -0600

    added table creation logic to table creation file

--- migrations/20160318094234_create_movie_table.js
+++ migrations/20160318094234_create_movie_table.js
@@ -1,8 +1,14 @@
 
 exports.up = function(knex, Promise) {
-  
+  return knex.schema.createTable('movies', function(table) {
+    table.increments();
+    table.string('director');
+    table.string('title');
+    table.decimal('rating', 3, 1);
+    table.string('description', 1000);
+  });
 };
 
 exports.down = function(knex, Promise) {
-  
+  return knex.schema.dropTable('movies');
 };
